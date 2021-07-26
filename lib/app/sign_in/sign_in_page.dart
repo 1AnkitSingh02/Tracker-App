67a3b064150eac096f474db41aaff17dd789db0f
1AnkitSingh02 <ankitsingh4301@gmail.com>
2021-07-26 21:25:35 +0530

    Initial Commit

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: Container(
+        color: Colors.black12,
+      ),
+    );
+  }
+}
